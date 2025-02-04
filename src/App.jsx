8d86d36ac4cf2a0875f39278e64fb797606d27a1
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2025-02-04 21:14:30 +0200

    First push

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Input } from "antd";
+function App() {
+
+  return (
+    <div>Hello
+      <h1 className="text-3xl font-bold underline ms-3">
+        Hello world!
+      </h1>
+      <Input placeholder="Basic usage" className="!w-1/2" />
+    </div>
+  )
+}
+
+export default App
